8577bbfb62cd0762ad669634be6d113de53beaf5
Edwin Löffler <edwin.frank.loeffler@gmail.com>
2026-04-07 19:54:28 +0200

    feat: change `HostId::name` to be human-readable (#1146)

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -36,7 +36,7 @@ pub use crate::host::custom::{Device as CustomDevice, Host as CustomHost, Stream
 /// SupportedOutputConfigs and all their necessary trait implementations.
 ///
 macro_rules! impl_platform_host {
-    ($($(#[cfg($feat: meta)])? $HostVariant:ident => $Host:ty),* $(,)?) => {
+    ($($(#[cfg($feat: meta)])? $HostVariant:ident $($HostName:literal)? => $Host:ty),* $(,)?) => {
         /// All hosts supported by CPAL on this platform.
         pub const ALL_HOSTS: &'static [HostId] = &[
             $(
@@ -186,11 +186,12 @@ macro_rules! impl_platform_host {
         }
 
         impl HostId {
+            /// Returns the human-readable host name.
             pub fn name(&self) -> &'static str {
                 match self {
                     $(
                         $(#[cfg($feat)])?
-                        HostId::$HostVariant => stringify!($HostVariant),
+                        HostId::$HostVariant => __cpal_select_host_name!($HostVariant, $($HostName)?),
                     )*
                 }
             }
@@ -198,7 +199,7 @@ macro_rules! impl_platform_host {
 
         impl std::fmt::Display for HostId {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "{}", self.name().to_lowercase())
+                write!(f, "{}", self.name().to_ascii_lowercase())
             }
         }
 
@@ -208,7 +209,7 @@ macro_rules! impl_platform_host {
             fn from_str(s: &str) -> Result<Self, Self::Err> {
                 $(
                     $(#[cfg($feat)])?
-                    if stringify!($HostVariant).eq_ignore_ascii_case(s) {
+                    if HostId::$HostVariant.name().eq_ignore_ascii_case(s) {
                         return Ok(HostId::$HostVariant);
                     }
                 )*
@@ -715,6 +716,15 @@ macro_rules! impl_platform_host {
     };
 }
 
+macro_rules! __cpal_select_host_name {
+    ($variant:ident, $name:literal) => {
+        $name
+    };
+    ($variant:ident,) => {
+        stringify!($variant)
+    };
+}
+
 #[cfg(any(
     target_os = "linux",
     target_os = "dragonfly",
@@ -777,8 +787,8 @@ mod platform_impl {
     impl_platform_host!(
         #[cfg(feature = "pipewire")] PipeWire => PipeWireHost,
         #[cfg(feature = "pulseaudio")] PulseAudio => PulseAudioHost,
-        #[cfg(feature = "jack")] Jack => JackHost,
-        Alsa => AlsaHost,
+        #[cfg(feature = "jack")] Jack "JACK" => JackHost,
+        Alsa "ALSA" => AlsaHost,
         #[cfg(feature = "custom")] Custom => super::CustomHost,
     );
 
@@ -812,7 +822,7 @@ mod platform_impl {
 
     impl_platform_host!(
         CoreAudio => CoreAudioHost,
-        #[cfg(all(feature = "jack", target_os = "macos"))] Jack => JackHost,
+        #[cfg(all(feature = "jack", target_os = "macos"))] Jack "JACK" => JackHost,
         #[cfg(feature = "custom")] Custom => super::CustomHost
     );
 
@@ -886,9 +896,9 @@ mod platform_impl {
     pub use crate::host::wasapi::Host as WasapiHost;
 
     impl_platform_host!(
-        #[cfg(feature = "asio")] Asio => AsioHost,
-        Wasapi => WasapiHost,
-        #[cfg(feature = "jack")] Jack => JackHost,
+        #[cfg(feature = "asio")] Asio "ASIO" => AsioHost,
+        Wasapi "WASAPI" => WasapiHost,
+        #[cfg(feature = "jack")] Jack "JACK" => JackHost,
         #[cfg(feature = "custom")] Custom => super::CustomHost,
     );
 
